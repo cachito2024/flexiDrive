92785f79173cce4931140697f965c16613b4962c
Anto Peretti <antonellaperetti98@gmail.com>
2026-02-16 11:16:05 -0300

    ahora sí guardo cambios de todos los microservicios

---
+++ microservicios/envio-service/src/models/envioXcomisionistaModel.js
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose';
+
+const envioXComisionistaSchema = new mongoose.Schema({
+  comisionistaId: { type: mongoose.Schema.Types.ObjectId, required: true },
+  envioId: { type: mongoose.Schema.Types.ObjectId, ref: 'Envio', required: true }, 
+  vehiculoId: { type: mongoose.Schema.Types.ObjectId, required: true }, // El que Marta elija al aceptar
+  fecha_asignacion: { type: Date, default: Date.now },
+  fecha_inicio: { type: Date },
+  fecha_fin: { type: Date },
+  estado_id: { 
+    type: String, 
+    enum: ['ASIGNADO', 'PENDIENTE', 'EN_RETIRO', 'EN_CAMINO', 'ENTREGADO', 'DEMORADO', 'CANCELADO','CANCELADO_RETORNO', 'DEVUELTO'],
+    default: 'PENDIENTE' 
+  }
+}, { timestamps: true });
+
+export default mongoose.model('EnvioXComisionista', envioXComisionistaSchema, 'envioXComisionista');
